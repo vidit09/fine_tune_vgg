1b15660705135eab7fa5c8da450ce167df528cab
Vidit <vidit@Mac-mini-de-Bennani-Smires.local>
2017-10-25 15:17:33 +0200

    only conv5 trainable

--- modified_vgg.py
+++ modified_vgg.py
@@ -16,7 +16,7 @@ def get_model(num_class):
 
     model = Model(vgg.input, x)
 
-    for layer in model.layers[:15]:
+    for layer in model.layers[:17]:
         print(layer)
         layer.trainable = False
 
